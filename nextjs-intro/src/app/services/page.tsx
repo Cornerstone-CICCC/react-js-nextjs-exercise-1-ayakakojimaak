8331eb88a957280800303cc5b294bae17ba2188d
ayakakojimaak <125942459+ayakakojimaak@users.noreply.github.com>
2025-06-03 12:37:34 -0700

    add pages

---
+++ nextjs-intro/src/app/services/page.tsx
@@ -0,0 +1,11 @@
+export default function Services() {
+  return (
+    <div className="max-w-4xl mx-auto">
+      <h1 className="text-4xl font-bold mb-6">Our Services</h1>
+      <p className="text-lg text-gray-700">
+        Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem
+        aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
+      </p>
+    </div>
+  );
+}
